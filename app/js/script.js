550075f498498e7b9a3264ce67a7577f29a2c931
SBdC <denominatu@hotmail.com>
2019-03-26 17:04:45 +0100

    finished burger  menu functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 
 
+const OVERLAY = document.getElementById('overlay');
 const TRIGGER = document.querySelector('.trigger');
 const CLOSE = document.querySelector('.burger');
 const BURGERBLACK= document.querySelector('.burger-black');
@@ -10,17 +11,23 @@ let screenReaderText = document.querySelector('.trigger .screen-reader-text');
 
 
 
+
 function revealMenu() {
-	console.log("hello world");
+ 
+	OVERLAY.classList.toggle('opaque');
     NAV.classList.toggle('menu-items');
     BURGERBLACK.classList.toggle('burger-white');
     BURGERWHITE.classList.toggle('burger-black');
     BURGERWHITE.classList.toggle('burger-white');
     MENUITEMS.classList.toggle('open');
-    TRIGGER.getAttribute('aria-expanded') == 'false' ? TRIGGER.setAttribute('aria-expanded', true) : TRIGGER.setAttribute('aria-expanded', false);
+    TRIGGER.getAttribute('aria-expanded') == 'false' ? CLOSE.setAttribute('aria-expanded', true) : CLOSE.setAttribute('aria-expanded', false);
     screenReaderText.innerHTML == 'Reveal menu' ? screenReaderText.innerHTML = 'Hide menu' : screenReaderText.innerHTML = 'Reveal menu';
+   
+
 }
 
-TRIGGER.addEventListener('click', revealMenu, false);
 
+//remeber to check the focus for aria
+OVERLAY.addEventListener('click', revealMenu, false);
+TRIGGER.addEventListener('click', revealMenu, false);
 CLOSE.addEventListener('click', revealMenu, false);
