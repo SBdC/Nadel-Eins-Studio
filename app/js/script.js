26baadb8a85f0b4fceefefb2b99b483e0a459d8c
SBdC <denominatu@hotmail.com>
2019-06-13 21:21:38 +0200

    fixed scrollToview and eslinted

--- app/js/script.js
+++ app/js/script.js
@@ -13,14 +13,14 @@ let imgs = Array.from(document.querySelectorAll(".zoom"));
 
 
 function revealMenu() {
-  OVERLAY.classList.toggle("opaque");
-  NAV.classList.toggle("menu-items");
-  BURGERBLACK.classList.toggle("burger-white");
-  BURGERWHITE.classList.toggle("burger-black");
-  BURGERWHITE.classList.toggle("burger-white");
-  MENUITEMS.classList.toggle("open");
-  TRIGGER.getAttribute("aria-expanded") == "false" ? CLOSE.setAttribute("aria-expanded", true) : CLOSE.setAttribute("aria-expanded", false);
-  screenReaderText.innerHTML == "Reveal menu" ? screenReaderText.innerHTML = "Hide menu" : screenReaderText.innerHTML = "Reveal menu";
+    OVERLAY.classList.toggle("opaque");
+    NAV.classList.toggle("menu-items");
+    BURGERBLACK.classList.toggle("burger-white");
+    BURGERWHITE.classList.toggle("burger-black");
+    BURGERWHITE.classList.toggle("burger-white");
+    MENUITEMS.classList.toggle("open");
+    TRIGGER.getAttribute("aria-expanded") == "false" ? CLOSE.setAttribute("aria-expanded", true) : CLOSE.setAttribute("aria-expanded", false);
+    screenReaderText.innerHTML == "Reveal menu" ? screenReaderText.innerHTML = "Hide menu" : screenReaderText.innerHTML = "Reveal menu";
 }
 
 points.forEach(read => read.addEventListener("click", revealMenu, false));
@@ -39,48 +39,48 @@ let erases = Array.from(document.querySelectorAll(".viewLess"));
 
 
 let revealText = (e) => {
-   
-  let dropDown = document.querySelector(`p[data-key="${e.target.id}"]`);
-   dropDown.style.display = "block";
-  let firstParent = e.target.parentElement;
-  let greatParent =firstParent.parentElement;
- let greatGreatParent =greatParent.parentElement;
-  greatGreatParent.style.flex="0 1 calc(100%)";
-   greatGreatParent.style.order="-1";
-  let button = document.getElementById(`${e.target.id}`);
-  button.style.display = "none";
-  let otherButton = document.querySelector(`button[data-type="${e.target.id}"]`);
-  otherButton.style.display = "block";
+
+    let dropDown = document.querySelector(`p[data-key="${e.target.id}"]`);
+    dropDown.style.display = "block";
+    let firstParent = e.target.parentElement;
+    let greatParent = firstParent.parentElement;
+    let greatGreatParent = greatParent.parentElement;
+    greatGreatParent.style.flex = "0 1 calc(100%)";
+    greatGreatParent.style.order = "-1";
+    let button = document.getElementById(`${e.target.id}`);
+    button.style.display = "none";
+    let otherButton = document.querySelector(`button[data-type="${e.target.id}"]`);
+    otherButton.style.display = "block";
 
 
-greatGreatParent.scrollIntoView({behavior: "smooth", block: "center",inline: "center"});
+    greatGreatParent.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
 
 
 };
 
 let hideText = (e) => {
-  let targetService = e.target.id;
-  const goUp = document.querySelector(`p[data-hide="${targetService }"]`);
-  goUp.style.display = "none";
-  goUp.style.transition = "all 2s";
+    let targetService = e.target.id;
+    const goUp = document.querySelector(`p[data-hide="${targetService }"]`);
+    goUp.style.display = "none";
+    goUp.style.transition = "all 2s";
 
-  let otherButton = document.getElementById(`${targetService }`);
-  otherButton.style.display = "none";
-  let button = document.querySelector(`button[data-type="${targetService }"]`);
-  button.style.display = "block";
+    let otherButton = document.getElementById(`${targetService }`);
+    otherButton.style.display = "none";
+    let button = document.querySelector(`button[data-type="${targetService }"]`);
+    button.style.display = "block";
 
-  let firstParent = e.target.parentElement;
-  let greatParent =firstParent.parentElement;
- let greatGreatParent =greatParent.parentElement;
-  greatGreatParent.style.flex="";
-  greatGreatParent.style.order="";
+    let firstParent = e.target.parentElement;
+    let greatParent = firstParent.parentElement;
+    let greatGreatParent = greatParent.parentElement;
+    greatGreatParent.style.flex = "";
+    greatGreatParent.style.order = "";
 
-  let targetServiveElemnt=document.getElementById(targetService);
-  let parentTargetServiveElemnt = targetServiveElemnt.parentElement;
-  parentTargetServiveElemnt.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+    let targetServiveElemnt = document.getElementById(targetService);
+    let parentTargetServiveElemnt = targetServiveElemnt.parentElement;
+    parentTargetServiveElemnt.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
 
-   
-    
+
+
 };
 
 
@@ -104,15 +104,15 @@ let captions = Array.from(document.querySelectorAll(".img-text"));
 
 function revealCaption() {
 
-  captions.forEach(caption => caption.classList.add("img-text-reveal"));
-  captions.forEach(caption => caption.classList.remove("img-text"));
+    captions.forEach(caption => caption.classList.add("img-text-reveal"));
+    captions.forEach(caption => caption.classList.remove("img-text"));
 
 }
 
 function hideCaption() {
 
-  captions.forEach(caption => caption.classList.remove("img-text-reveal"));
-  captions.forEach(caption => caption.classList.add("img-text"));
+    captions.forEach(caption => caption.classList.remove("img-text-reveal"));
+    captions.forEach(caption => caption.classList.add("img-text"));
 
 }
 
@@ -122,12 +122,12 @@ SLIDESHOW.addEventListener("mouseover", revealCaption, false);
 
 // Next/previous controls
 function plusSlides() {
-  showSlides(slideIndex += 1);
+    showSlides(slideIndex += 1);
 
 }
 
 function minusSlides() {
-  showSlides(slideIndex -= 1);
+    showSlides(slideIndex -= 1);
 
 
 
@@ -137,7 +137,7 @@ function minusSlides() {
 
 function currentSlide(n) {
 
-  showSlides(slideIndex = `${n.target.dataset.id}`);
+    showSlides(slideIndex = `${n.target.dataset.id}`);
 }
 
 
@@ -145,23 +145,23 @@ function currentSlide(n) {
 
 function showSlides(n) {
 
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dots");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dots");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 
 }
 
@@ -185,15 +185,15 @@ const BODY = document.getElementsByTagName("BODY")[0];
 
 // When the user clicks on <span> (x), close the modal
 SPAN.onclick = function() {
-  MODAL.style.display = "none";
+    MODAL.style.display = "none";
 };
 
 
 let zoomImage = (e) => {
-  MODAL.style.display = "block";
-  modalImg.src = e.target.src;
-  captionText.innerHTML = e.target.nextElementSibling.innerHTML;
-  BODY.style.overflow = "hidden";
+    MODAL.style.display = "block";
+    modalImg.src = e.target.src;
+    captionText.innerHTML = e.target.nextElementSibling.innerHTML;
+    BODY.style.overflow = "hidden";
 };
 imgs.forEach(img => img.addEventListener("click", zoomImage, false));
 
@@ -202,54 +202,54 @@ imgs.forEach(img => img.addEventListener("click", zoomImage, false));
 //load more music albums
 
 let parent = document.querySelector("ul"),
-  items = parent.querySelectorAll("li"),
-  loadMoreBtn = document.querySelector("#loadMore"),
-  loadLessBtn = document.querySelector("#loadless"),
-  maxItems = 10,
-  hiddenClass = "visually-hidden",
-  showClass = "visually";
+    items = parent.querySelectorAll("li"),
+    loadMoreBtn = document.querySelector("#loadMore"),
+    loadLessBtn = document.querySelector("#loadless"),
+    maxItems = 10,
+    hiddenClass = "visually-hidden",
+    showClass = "visually";
 
 [].forEach.call(items, function(item, idx) {
-  if (idx > maxItems - 1) {
-    item.classList.add(hiddenClass);
-  }
+    if (idx > maxItems - 1) {
+        item.classList.add(hiddenClass);
+    }
 });
 
 loadMoreBtn.addEventListener("click", function() {
 
-  [].forEach.call(document.querySelectorAll("." + hiddenClass), function(item, idx) {
-    console.log(item);
-    if (idx < maxItems - 1) {
-      item.classList.remove(hiddenClass);
-      item.classList.add(showClass);
-    }
+    [].forEach.call(document.querySelectorAll("." + hiddenClass), function(item, idx) {
+        console.log(item);
+        if (idx < maxItems - 1) {
+            item.classList.remove(hiddenClass);
+            item.classList.add(showClass);
+        }
 
-    if (document.querySelectorAll("." + hiddenClass).length === 0) {
-      loadMoreBtn.style.display = "none";
-      loadLessBtn.style.display = "block";
-    }
+        if (document.querySelectorAll("." + hiddenClass).length === 0) {
+            loadMoreBtn.style.display = "none";
+            loadLessBtn.style.display = "block";
+        }
 
-  });
+    });
 
 });
 
 
 loadLessBtn.addEventListener("click", function() {
 
-  [].forEach.call(document.querySelectorAll("." + showClass), function(item, idx) {
-    console.log(item);
-    if (idx < maxItems - 1) {
-      item.classList.remove(showClass);
-      item.classList.add(hiddenClass);
+    [].forEach.call(document.querySelectorAll("." + showClass), function(item, idx) {
+        console.log(item);
+        if (idx < maxItems - 1) {
+            item.classList.remove(showClass);
+            item.classList.add(hiddenClass);
 
-    }
+        }
 
-    if (document.querySelectorAll("." + showClass).length === 0) {
-      loadMoreBtn.style.display = "block";
-      loadLessBtn.style.display = "none";
-    }
+        if (document.querySelectorAll("." + showClass).length === 0) {
+            loadMoreBtn.style.display = "block";
+            loadLessBtn.style.display = "none";
+        }
 
-  });
+    });
 
 });
 
@@ -265,28 +265,28 @@ let isScrolling;
 // Listen for scroll events
 window.addEventListener("scroll", function() {
 
-  // Clear our timeout throughout the scroll
-  window.clearTimeout(isScrolling);
+    // Clear our timeout throughout the scroll
+    window.clearTimeout(isScrolling);
 
 
-  if (backTop.classList.contains("btn-entrance")) {
-    backTop.classList.remove("btn-entrance");
-    backTop.classList.add("btn-exit");
-    setTimeout(function() {
-      backTop.style.display = "none";
-    }, 150);
+    if (backTop.classList.contains("btn-entrance")) {
+        backTop.classList.remove("btn-entrance");
+        backTop.classList.add("btn-exit");
+        setTimeout(function() {
+            backTop.style.display = "none";
+        }, 150);
 
-  }
-
-  // Set a timeout to run after scrolling ends
-  isScrolling = setTimeout(function() {
-    if (window.pageYOffset > 400) {
-      backTop.classList.remove("btn-exit");
-      backTop.classList.add("btn-entrance");
-      backTop.style.display = "block";
     }
 
-  }, 66);
+    // Set a timeout to run after scrolling ends
+    isScrolling = setTimeout(function() {
+        if (window.pageYOffset > 400) {
+            backTop.classList.remove("btn-exit");
+            backTop.classList.add("btn-entrance");
+            backTop.style.display = "block";
+        }
+
+    }, 66);
 
 }, false);
 
@@ -294,7 +294,7 @@ window.addEventListener("scroll", function() {
 backTop.addEventListener("click", backToTop);
 
 function backToTop() {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 }
 
 
@@ -323,12 +323,12 @@ let impressumTextContent = `<p class="impressum-text"> Nadel Eins Studio Berlin 
   Nadel Eins wird diese ohne die Einwilligung des Nutzers nicht an Dritte weitergeben</p>`;
 
 
-function showImpressum (){
+function showImpressum() {
 
-  IMPRESSUM.getAttribute("aria-expanded") == "false" ? IMPRESSUM.setAttribute("aria-expanded", true) : IMPRESSUM.setAttribute("aria-expanded", false);
-  impressumText.innerHTML ==  impressumTextContent ? impressumText.innerHTML = "" : impressumText.innerHTML = impressumTextContent;
-  impressumArrow.style.transform = (impressumArrow.style.transform == "rotate(90deg)") ? "rotate(270deg)" : "rotate(90deg)";
-  window.scrollTo(0,document.body.scrollHeight)? window.scrollTo(IMPRESSUM) : window.scrollTo(0,document.body.scrollHeight);
+    IMPRESSUM.getAttribute("aria-expanded") == "false" ? IMPRESSUM.setAttribute("aria-expanded", true) : IMPRESSUM.setAttribute("aria-expanded", false);
+    impressumText.innerHTML == impressumTextContent ? impressumText.innerHTML = "" : impressumText.innerHTML = impressumTextContent;
+    impressumArrow.style.transform = (impressumArrow.style.transform == "rotate(90deg)") ? "rotate(270deg)" : "rotate(90deg)";
+    window.scrollTo(0, document.body.scrollHeight) ? window.scrollTo(IMPRESSUM) : window.scrollTo(0, document.body.scrollHeight);
 
 }
 
@@ -350,51 +350,52 @@ const smile = document.querySelector(".smile");
 const smileBar = document.querySelector(".smileBar");
 
 
-const keys = Array.from(document.querySelectorAll('.text-key'));
-const faces = Array.from(document.querySelectorAll('.faces'));
-const droppings = Array.from(document.querySelectorAll('.dropping'));
-const serviceSvgs = Array.from(document.querySelectorAll('.service-svg'));
-const serviceTexts = Array.from(document.querySelectorAll('.service-text'));
+const clouds = document.getElementById("clouds");
+const drops = document.getElementById("drops");
 
-const gridContainer = document.getElementById("grid-container")
+const keys = Array.from(document.querySelectorAll(".text-key"));
+const faces = Array.from(document.querySelectorAll(".faces"));
+const droppings = Array.from(document.querySelectorAll(".dropping"));
+const serviceSvgs = Array.from(document.querySelectorAll(".service-svg"));
 
+const title = document.getElementById("title-page");
 
 
 //thunders gets drawn
 
 let thunderOn = () => {
 
- 
+
     face.style.fill = "red";
-    thunder.style.stroke ="black";
+    thunder.style.stroke = "black";
     thunder.classList.add("thunder-happens");
-    smile.style.stroke ="transparent";
-    smileBar.style.fill ="transparent";
-    smileBar.style.stroke ="transparent";
-    surprise.style.fill ="white";
-    surprise.style.stroke ="black";
-    title.style.color="white";
-    document.body.style.background="snow";
+    smile.style.stroke = "transparent";
+    smileBar.style.fill = "transparent";
+    smileBar.style.stroke = "transparent";
+    surprise.style.fill = "white";
+    surprise.style.stroke = "black";
+    title.style.color = "white";
+    document.body.style.background = "snow";
     // city.style.background="snow";
-  
+
 };
 
-let thunderOff= () => {
+let thunderOff = () => {
 
 
     face.style.fill = "yellow";
     thunder.classList.remove("thunder-happens");
-    thunder.style.stroke ="transparent";
-    smile.style.stroke ="black";
-    smileBar.style.fill ="black";
-    smileBar.style.stroke ="black";
-    surprise.style.fill ="transparent";
-    surprise.style.stroke ="transparent";
+    thunder.style.stroke = "transparent";
+    smile.style.stroke = "black";
+    smileBar.style.fill = "black";
+    smileBar.style.stroke = "black";
+    surprise.style.fill = "transparent";
+    surprise.style.stroke = "transparent";
 
-   document.body.style.background="";
-    
-    title.style.color="red";
-    
+    document.body.style.background = "";
+
+    title.style.color = "red";
+
 };
 
 
@@ -414,44 +415,43 @@ faces.forEach(face => face.addEventListener("mouseout", thunderOff, false));
 //cloud-menu
 
 
-let explodeAndReveal = (e) =>  {
-  
-  let clickedCloud = e.target;
-  let clickedCloudChild = clickedCloud.firstChild.textContent;
-  let cloudLink = document.getElementById(clickedCloudChild);
+let explodeAndReveal = (e) => {
+
+    let clickedCloud = e.target;
+    let clickedCloudChild = clickedCloud.firstChild.textContent;
+    let cloudLink = document.getElementById(clickedCloudChild);
 
 
-  rain();
-
- 
-
- let goLink =() =>{
-  cloudLink.scrollIntoView();
- }
+    rain();
 
 
- setTimeout(goLink, 1000);
 
-}
+    let goLink = () => {
+        cloudLink.scrollIntoView();
+    };
 
-keys.forEach(key => key.addEventListener('click', explodeAndReveal, false));
+
+    setTimeout(goLink, 1000);
+
+};
+
+keys.forEach(key => key.addEventListener("click", explodeAndReveal, false));
 
 
 
 
 function rain() {
-
-
-  clouds.style.stroke="transparent";
-  drops.style.stroke="#449AFF";
-  droppings.forEach(dropping=> dropping.classList.add("drops-go-down"));
-  console.log(drops)
  
-  keys.forEach(key=> key.classList.remove("text-on-cloud"));
-  keys.forEach(key=> key.classList.add("text-off"));
+    clouds.style.stroke = "transparent";
+    drops.style.stroke = "#449AFF";
+    droppings.forEach(dropping => dropping.classList.add("drops-go-down"));
   
 
-  setTimeout(cloudySky, 5000);
+    keys.forEach(key => key.classList.remove("text-on-cloud"));
+    keys.forEach(key => key.classList.add("text-off"));
+
+
+    setTimeout(cloudySky, 5000);
 
 
 }
@@ -459,12 +459,12 @@ function rain() {
 
 function cloudySky() {
 
-  clouds.style.stroke="#449AFF";
-  drops.style.stroke="transparent"
-  droppings.forEach(dropping=> dropping.classList.remove("drops-go-down"));
+    clouds.style.stroke = "#449AFF";
+    drops.style.stroke = "transparent";
+    droppings.forEach(dropping => dropping.classList.remove("drops-go-down"));
 
-  keys.forEach(key=> key.classList.remove("text-off"));
-  keys.forEach(key=> key.classList.add("text-on-cloud"));
+    keys.forEach(key => key.classList.remove("text-off"));
+    keys.forEach(key => key.classList.add("text-on-cloud"));
 
 
 }
@@ -473,40 +473,33 @@ function cloudySky() {
 
 
 let highlightSvg = (e) => {
-let mainEl = e.target.dataset.id;
-let svgText = document.querySelector(`text[id="${mainEl}"]`);
+    let mainEl = e.target.dataset.id;
+    let svgText = document.querySelector(`text[id="${mainEl}"]`);
 
-let svgPaths = Array.from(document.querySelectorAll(`path[data-id="${mainEl}"]`));
-svgPaths.forEach(svgPath=> svgPath.style.stroke="red");
+    let svgPaths = Array.from(document.querySelectorAll(`path[data-id="${mainEl}"]`));
+    svgPaths.forEach(svgPath => svgPath.style.stroke = "red");
 
- svgText.classList.remove("text-off");
- svgText.classList.add("text-on");
+    svgText.classList.remove("text-off");
+    svgText.classList.add("text-on");
 
 
 
-}
+};
 
 let highlightSvgOFF = (e) => {
 
-let mainEl = e.target.dataset.id;
-let svgText = document.querySelector(`text[id="${mainEl}"]`);
-let svgPaths = Array.from(document.querySelectorAll(`path[data-id="${mainEl}"]`));
-svgPaths.forEach(svgPath=> svgPath.style.stroke="black");
+    let mainEl = e.target.dataset.id;
+    let svgText = document.querySelector(`text[id="${mainEl}"]`);
+    let svgPaths = Array.from(document.querySelectorAll(`path[data-id="${mainEl}"]`));
+    svgPaths.forEach(svgPath => svgPath.style.stroke = "black");
 
 
-svgText.classList.add("text-off");
-svgText.classList.remove("text-on");
+    svgText.classList.add("text-off");
+    svgText.classList.remove("text-on");
 
-}
+};
 
 
 
 serviceSvgs.forEach(serviceSvg => serviceSvg.addEventListener("mouseover", highlightSvg, false));
 serviceSvgs.forEach(serviceSvg => serviceSvg.addEventListener("mouseout", highlightSvgOFF, false));
-
-
-
-
-
-
-
